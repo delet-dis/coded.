1471d3ca1ae8cc7db4e4a159cca2db884c299051
delet-dis <efimov.036@gmail.com>
2022-04-18 11:07:14 +0700

    🛬 added OnboardingActivity

---
+++ app/src/main/java/com/hits/coded/presentation/activities/onboardingActivity/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.hits.coded.presentation.activities.onboardingActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hits.coded.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
