9c5591520640aec3fb6b5e075e418a3f5113db1f
Artem Trushkin <at.120@ya.ru>
2022-04-25 23:59:45 +0700

    InterpreterCaller realization

---
+++ app/src/main/java/com/hits/coded/data/modules/InterpreterCallerModule.kt
@@ -0,0 +1,35 @@
+package com.hits.coded.data.modules
+
+import com.hits.coded.data.models.interpreterCaller.InterpreterCallerUseCases
+import com.hits.coded.data.repositoriesImplementations.InterpreterCallerRepositoryImplementation
+import com.hits.coded.domain.repositories.InterpreterCallerRepository
+import com.hits.coded.domain.useCases.interpreterCaller.CallInterpreterUseCase
+import com.hits.coded.domain.useCases.interpreterCaller.GetExecutionResultsUseCase
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class InterpreterCallerModule {
+    @Binds
+    abstract fun bindInterpreterCallerRepository(
+        interpreterCallerRepository: InterpreterCallerRepositoryImplementation
+    ): InterpreterCallerRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideInterpreterCallerUseCases(
+            interpreterCallerRepository: InterpreterCallerRepository
+        ): InterpreterCallerUseCases =
+            InterpreterCallerUseCases(
+                CallInterpreterUseCase(interpreterCallerRepository),
+                GetExecutionResultsUseCase(interpreterCallerRepository)
+            )
+    }
+}
