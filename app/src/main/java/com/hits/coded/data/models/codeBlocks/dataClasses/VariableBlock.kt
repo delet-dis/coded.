3a43644fc4d3e38723e0c0f2b6519445685ade87
delet-dis <efimov.036@gmail.com>
2022-04-25 15:44:37 +0700

    ⬆️ updated data structures

--- app/src/main/java/com/hits/coded/data/models/codeBlocks/dataClasses/VariableBlock.kt
+++ app/src/main/java/com/hits/coded/data/models/codeBlocks/dataClasses/VariableBlock.kt
@@ -7,11 +7,12 @@ import com.hits.coded.data.models.codeBlocks.types.subBlocks.VariableBlockType
 import com.hits.coded.data.models.types.VariableType
 
 data class VariableBlock(
+    override val id: Int,
     override val variableBlockType: VariableBlockType,
     override val variableName: String,
     override val variableType: VariableType,
     override val isArray: Boolean,
     override val variableValue: Any,
     override val type: BlockType = BlockType.VARIABLE,
-    override val nestedBlocks: Array<BlockBase>? = null,
+    override val nestedBlocks: Array<BlockBase>? = null
 ) : VariableBlockBase()
