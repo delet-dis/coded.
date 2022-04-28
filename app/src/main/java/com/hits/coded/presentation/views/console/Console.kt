ed5311e0ca70dbf5c70a26c18418a37dcc44a079
delet-dis <efimov.036@gmail.com>
2022-04-28 18:50:32 +0700

    🖥 added basic console files

---
+++ app/src/main/java/com/hits/coded/presentation/views/console/Console.kt
@@ -0,0 +1,15 @@
+package com.hits.coded.presentation.views.console
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class Console constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding:
+}
