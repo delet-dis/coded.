add6ecf50c3ad41b2587501a6f4c6d70d02682d1
Artem Trushkin <at.120@ya.ru>
2022-04-30 12:47:21 +0700

    ability to set color has been added

--- app/src/main/java/com/hits/coded/domain/useCases/console/WriteToConsoleUseCase.kt
+++ app/src/main/java/com/hits/coded/domain/useCases/console/WriteToConsoleUseCase.kt
@@ -1,7 +1,8 @@
 package com.hits.coded.domain.useCases.console
 
+import android.graphics.Color
 import com.hits.coded.domain.repositories.ConsoleRepository
 
 class WriteToConsoleUseCase(private val consoleRepository: ConsoleRepository) {
-    fun writeToConsole(input: String) = consoleRepository.writeToConsole(input)
+    fun writeToConsole(input: String, color: Int = Color.WHITE) = consoleRepository.writeToConsole(input, color)
 }
