0efcfb5980f6d8b2fad24c5b2398389ae82e3782
delet-dis <efimov.036@gmail.com>
2022-05-04 18:37:34 +0700

    ⌛️ in process of expression block adding

--- app/src/main/java/com/hits/coded/data/models/codeBlocks/dataClasses/VariableBlock.kt
+++ app/src/main/java/com/hits/coded/data/models/codeBlocks/dataClasses/VariableBlock.kt
@@ -7,10 +7,10 @@ import com.hits.coded.data.models.codeBlocks.types.subBlocks.VariableBlockType
 import com.hits.coded.data.models.heap.dataClasses.StoredVariable
 
 data class VariableBlock(
-    override val variableBlockType: VariableBlockType? = null,
+    override var variableBlockType: VariableBlockType? = null,
     override var variableParams: StoredVariable? = null,
     override var id: Int? = null,
-    override var nestedBlocks: Array<BlockBase>? = null,
     override var valueToSet: Any? = null,
+    override var nestedBlocks: Array<BlockBase>? = null,
     override val type: BlockType = BlockType.VARIABLE,
 ) : VariableBlockBase()
