8c5ed2120422382ff9c9713a8c220270028d1a31
Artem Trushkin <at.120@ya.ru>
2022-05-05 18:38:50 +0700

    MultiDimensionalArrays realisation

--- app/src/main/java/com/hits/coded/data/implementations/bases/arrayBase/MultiDimensionalArray.kt
+++ app/src/main/java/com/hits/coded/data/implementations/bases/arrayBase/MultiDimensionalArray.kt
@@ -4,10 +4,65 @@ import com.hits.coded.data.models.arrays.bases.ArrayBase
 import com.hits.coded.data.models.heap.dataClasses.StoredVariable
 import com.hits.coded.data.models.interpreterException.dataClasses.InterpreterException
 import com.hits.coded.data.models.sharedTypes.ExceptionType
+import com.hits.coded.data.models.sharedTypes.VariableType
 
 class MultiDimensionalArray(): ArrayBase() {
+
     override fun parseString(inputString: String): ArrayBase {
-        TODO("Not yet implemented")
+        val parsedArray = MultiDimensionalArray()
+        var arrayFound = false
+        var openedBrackets = 0
+        var arrayString = ""
+
+        for (char in inputString) {
+            if (char == '[') {
+                arrayFound = true
+                openedBrackets++
+            }
+
+            else if (char == ']') {
+                openedBrackets--
+            }
+
+            else if (arrayFound) {
+                arrayString += char
+            }
+
+
+            if (arrayFound && openedBrackets == 0) {
+                arrayFound = false
+                var nestedArray = constructByType(deductType(arrayString))
+                nestedArray = nestedArray.parseString(arrayString)
+                parsedArray.push(nestedArray)
+                arrayString = ""
+            }
+        }
+
+        return parsedArray
+    }
+
+    private fun deductType(arrayString: String): VariableType {
+        var type = VariableType.BOOLEAN
+        var isPrevDigit = false
+        for (char in arrayString) {
+            if (char == '[')
+                return VariableType.ARRAY
+
+            if (char == '"')
+                return VariableType.STRING
+
+            else if (char.isDigit()) {
+                isPrevDigit = true
+                if (char >= '2' || char <= '9') {
+                    type = VariableType.INT
+                }
+            }
+
+            else if (isPrevDigit && char == '.')
+                type = VariableType.DOUBLE
+        }
+
+        return type
     }
 
     override fun push(value: Any) {
@@ -17,7 +72,7 @@ class MultiDimensionalArray(): ArrayBase() {
                 ExceptionType.TYPE_MISMATCH
             )
 
-        //TODO: array type
-        array.add(StoredVariable(null, null, true, value))
+
+        array.add(StoredVariable(null, VariableType.ARRAY, true, value))
     }
 }
