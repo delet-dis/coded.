6713993273a246cb97dc815f29787bc94d635f3c
delet-dis <efimov.036@gmail.com>
2022-05-12 09:36:38 +0700

    🔁 added loop block

--- app/src/main/java/com/hits/coded/data/models/codeBlocks/bases/subBlocks/LoopBlockBase.kt
+++ app/src/main/java/com/hits/coded/data/models/codeBlocks/bases/subBlocks/LoopBlockBase.kt
@@ -1,10 +1,9 @@
 package com.hits.coded.data.models.codeBlocks.bases.subBlocks
 
 import com.hits.coded.data.models.codeBlocks.bases.BlockBase
-import com.hits.coded.data.models.codeBlocks.bases.subBlocks.condition.ConditionBlockBase
 import com.hits.coded.data.models.codeBlocks.types.subBlocks.LoopBlockType
 
 abstract class LoopBlockBase : BlockBase() {
-    abstract val loopBlockType: LoopBlockType
-    abstract val conditionBlock: ConditionBlockBase?
+    abstract val loopBlockType: LoopBlockType?
+    abstract val conditionBlock: Any?
 }
